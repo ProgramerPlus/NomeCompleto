b5eea2e1fd943de11f909d7ff8b8555acc5538d0
ProgramerPlus <hipitelo@outlook.com>
2025-09-24 22:39:03 -0300

    Exercício finalizado

--- Program.cs
+++ Program.cs
@@ -2,18 +2,20 @@
 
 Console.Write("Digite seu primeiro nome: ");
 nome = Console.ReadLine()!;
-//Configuração para a primeira letra do nome aparecer maiúscula automaticamente
+//Configuração para a primeira letra do nome aparecer maiúscula automaticamente e impedir minúsculas após
 inicial = nome.Substring(0, 1);
 inicial = inicial.ToUpper();
 nome = nome.Remove(0, 1);
+nome = nome.ToLower();
 nome = inicial + nome;
 
 Console.Write("Digite seu segundo nome: ");
 sobrenome = Console.ReadLine()!;
-//Configuração para a primeira letra do nome aparecer maiúscula automaticamente
+//Configuração para a primeira letra do nome aparecer maiúscula automaticamente e impedir minúsculas após
 inicial=sobrenome.Substring(0,1);
 inicial = inicial.ToUpper();
 sobrenome = sobrenome.Remove(0, 1);
+sobrenome = sobrenome.ToLower();
 sobrenome = inicial + sobrenome;
 
 Console.WriteLine($"Nome completo: {nome} {sobrenome}");
